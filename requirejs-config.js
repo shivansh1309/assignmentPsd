d783d0bfac52bbe46a477c59021093a83425c387
shivansh1309 <shivanshpradhan22@gmail.com>
2019-03-27 19:51:07 +0530

    27 March 2019 Last

---
+++ requirejs-config.js
@@ -0,0 +1,8 @@
+var config = {
+    map: {
+        '*': {
+            'cookiecode/jquery':        'js/jquery-3.3.1',
+            'owlcarousel':              'js/owl-carousel/owlcarousel'
+        }
+    }
+};
