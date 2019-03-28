442bbcdb4dea27a884c4854c6c87d54d282da49c
shivansh1309 <shivanshpradhan22@gmail.com>
2019-03-28 16:15:43 +0530

    28 March Evening

--- web/js/slider.js
+++ web/js/slider.js
@@ -1,18 +1,9 @@
-$(document).ready(function() {
+jQuery(document).ready(function($) {
  
-    $(".slide").owlCarousel({
-   
-        navigation : true, // Show next and prev buttons
-   
-        slideSpeed : 300,
-        paginationSpeed : 400,
-   
-        items : 1, 
-        itemsDesktop : false,
-        itemsDesktopSmall : false,
-        itemsTablet: false,
-        itemsMobile : false
-   
-    });
-   
-  });
+    var owl = $(".slide");
+    owl.owlCarousel({
+        items : 1,
+        navigation:true,
+    }); 
+
+});
